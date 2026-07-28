4638c843b977eaca3e0992e7c28f6cdc20b058aa
Tanu Choudhary <tanujpr007@gmail.com>
2026-07-28 19:19:40 +0530

    Time: 47 ms (50.55%) | Memory: 74.8 MB (39.78%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.length();
-        sort(s.begin(),s.end());
+        string t = "";
+        char c=-1;
         vector<int>v(26,0);
         for(char c:s) v[c-'a']++;
-        int j = 0,k = n-1;
         for(int i=0;i<26;i++){
             if(v[i]%2!=0) {
-                s[n/2]='a'+i;
+                c = 'a'+i;
                 v[i]--;
-                i--;
             }
-            else {
-                while(v[i]>0){
-                    s[j]='a'+i;
-                    s[k]='a'+i;
-                    j++;k--;
-                    v[i]-=2;
-                }
-            }
+            for(int j=0;j<v[i]/2;j++) t+='a'+i;
+            
         }
-        // string help = t;
-        // reverse(help.begin(),help.end());
-        // t +=help;
-        // if(c!=-1) t+=c;
-        // return t;
-        return s;
+        string help = t;
+        reverse(help.begin(),help.end());
+        
+        if(c!=-1) return t+c+help;
+        return t+help;
     }
 };
