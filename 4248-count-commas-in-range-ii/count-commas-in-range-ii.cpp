6d52a889f3fac809d2e3e48f2a7c1c5f4fe844d0
Tanu Choudhary <tanujpr007@gmail.com>
2026-09-09 18:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.33%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if (n < 1000) return 0;
+        long long ans = 0;
+        for(long long i = 1000; i <= n; i = i*1000) ans += (n-i+1);
+        return ans;
+    }
+};
