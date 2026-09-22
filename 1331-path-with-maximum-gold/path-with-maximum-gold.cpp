1f107f1cd6d8478453624805a8c7994231fa13ce
Tanu Choudhary <tanujpr007@gmail.com>
2026-09-22 18:52:27 +0530

    Time: 139 ms (48.93%) | Memory: 10.1 MB (70.91%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&grid,int r,int c){
+    int dfs(vector<vector<int>>&grid,int r,int c,vector<int>&dr,vector<int>&dc){
         //vst[r][c] = 1;
         int mx = 0;
         int prev = grid[r][c];
         grid[r][c] = 0;
-        vector<int>dr = {-1,0,1,0};
-        vector<int>dc = {0,-1,0,1};
         for(int i=0;i<4;i++){
             int nr = r + dr[i];
             int nc = c + dc[i];
             if(nr>=0 && nr<grid.size() && nc>=0 && nc<grid[0].size() && grid[nr][nc]) {
-                mx = max(dfs(grid,nr,nc),mx);
+                mx = max(dfs(grid,nr,nc,dr,dc),mx);
                 //std::cout<<val;
                 //dfs(grid,val,nr,nc);
             }
@@ -23,11 +21,13 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
+        vector<int>dr = {-1,0,1,0};
+        vector<int>dc = {0,-1,0,1};
         //vector<vector<int>>vst(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int val = 0;
-                if(grid[i][j]) ans = max(ans,dfs(grid,i,j));
+                if(grid[i][j]) ans = max(ans,dfs(grid,i,j,dr,dc));
             }
         }
         return ans;
